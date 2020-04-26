f8a2f36bdaf3e2e1e7f89a508c3d3a860be78fbf
Developer <developer@Mac-de-Developer.local>
2020-04-26 14:08:29 -0700

    creating bottom tabs navigations and header navigation paper react native

--- app/navigation/rootNavigator.tsx
+++ app/navigation/rootNavigator.tsx
@@ -4,7 +4,7 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import { DefaultTheme, DarkTheme } from "@react-navigation/native";
 import { useTheme } from "react-native-paper";
 
-import { FeedStack } from "./stack";
+import { StackNavigator } from "./stack";
 import { DrawerContent } from "./drawerContent";
 
 const Drawer = createDrawerNavigator();
@@ -16,7 +16,7 @@ export const RootNavigator = () => {
   return (
     <NavigationContainer theme={navigationTheme}>
       <Drawer.Navigator drawerContent={(props) => <DrawerContent {...props} />}>
-        <Drawer.Screen name="Home" component={FeedStack} />
+        <Drawer.Screen name="Home" component={StackNavigator} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
